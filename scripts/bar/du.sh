a624548cb11c077ed69ba5dfaa2fa57c0832d8c3
linarcx <linarcx@riseup.net>
2020-06-14 13:19:30 +0430

    update du.sh

--- scripts/bar/du.sh
+++ scripts/bar/du.sh
@@ -4,6 +4,6 @@ du () {
     HOMEFREE=$(df -h | grep '/dev/sda3' | awk '{print $4}')
     DFREE=$(df -h | grep '/dev/sda5' | awk '{print $4}')
     EFREE=$(df -h | grep '/dev/sda6' | awk '{print $4}')
-    printf "Home:%s E:%s D:%s" "$HOMEFREE" "$DFREE" "$EFREE"
+    printf "Home:%s D:%s E:%s" "$HOMEFREE" "$DFREE" "$EFREE"
 }
 du
