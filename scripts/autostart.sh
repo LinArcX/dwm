ffa0a2b9a4a895a1e753360a19e1b679ca51458d
linarcx <linarcx@riseup.net>
2020-07-11 21:53:11 +0430

    Check if mpv is running, don't run slock

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -8,9 +8,14 @@ setxkbmap -model pc105 -layout us,ir -option grp:alt_shift_toggle &
 st &
 firefox &
 dunst &
-while sleep 600; do slock; done &
+
+while sleep 600; do
+    pgrep mpv
+    if [ $? -ne 0 ]; then
+        slock;
+    fi;
+done &
 
 #/usr/libexec/policykit-1-pantheon/io.elementary.desktop.agent-polkit &
 #watch -n 20 '/usr/local/bin/slock' &>/dev/null &
-#watch -n 20 /usr/local/bin/slock &
 #> /dev/null 2>&1
