b003cb4626c9d8636bcb77f7f9e30037ae7739df
linarcx <linarcx@riseup.net>
2020-07-11 20:39:53 +0430

    add 600 seconds dalay and after that lock the screen.

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -4,8 +4,13 @@ feh --bg-fill /home/linarcx/Pictures/sierra.jpg &
 setxkbmap -model pc105 -layout us,ir -option grp:alt_shift_toggle &
 
 ./bar.sh &
-#/usr/libexec/policykit-1-pantheon/io.elementary.desktop.agent-polkit &
 
 st &
 firefox &
 dunst &
+while sleep 600; do slock; done &
+
+#/usr/libexec/policykit-1-pantheon/io.elementary.desktop.agent-polkit &
+#watch -n 20 '/usr/local/bin/slock' &>/dev/null &
+#watch -n 20 /usr/local/bin/slock &
+#> /dev/null 2>&1
