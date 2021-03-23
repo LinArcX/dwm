4e9fed36c35e0dd45930f6378cbb8a2af703a2fa
linarcx <linarcx@riseup.net>
2021-03-23 16:39:06 +0430

    :

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -1,19 +1,4 @@
 #!/bin/sh
-numlockx on &
-display -backdrop -background '#3f3f3f' -window root /home/linarcx/Pictures/sierra.jpg
+display -backdrop -background '#3f3f3f' -window root /mnt/D/images/andrea-rodriguez.jpg
 setxkbmap -model pc105 -layout us,ir -option grp:alt_shift_toggle &
-
 ./bar.sh &
-
-st &
-firefox &
-dunst &
-emacs --daemon
-emacsclient -s /var/run/user/1000/emacs/server -c -n -a emacs &
-
-while sleep 600; do
-    pgrep zathura
-    if [ $? -eq 0 ]; then
-        slock;
-    fi;
-done &
